ddb660d4139931f4b86de39498c1a093140ba1df
Phan Trọng Nhân <92132592+PTN407@users.noreply.github.com>
2023-06-20 01:34:31 +0700

    Update norec_sentiment.py

--- examples/sentiment/norec_sentiment.py
+++ examples/sentiment/norec_sentiment.py
@@ -10,47 +10,47 @@ from collections import defaultdict
 class FullSentimentAnnotator(CombinedAnnotator):
     """Annotation based on the heuristic"""
 
-    def add_all(self):
+    def add_all(self, root):
         """Adds all implemented annotation functions, models and filters"""
 
         print("Loading lexicon functions")
-        self.add_lexicons()
+        self.add_lexicons(root)
         print("Loading learned sentiment model functions")
-        self.add_ml_models()
+        self.add_ml_models(root)
 
         return self
 
-    def add_lexicons(self):
+    def add_lexicons(self, root):
         """Adds Spacy NER models to the annotator"""
 
-        self.add_annotator(LexiconAnnotator("norsent_forms", "../data/sentiment/lexicons/norsentlex/Fullform"))
-        self.add_annotator(LexiconAnnotator("norsent_lemma", "../data/sentiment/lexicons/norsentlex/Lemma"))
+        self.add_annotator(LexiconAnnotator("norsent_forms", root + "data/sentiment/lexicons/norsentlex/Fullform"))
+        self.add_annotator(LexiconAnnotator("norsent_lemma", root + "data/sentiment/lexicons/norsentlex/Lemma"))
 
-        self.add_annotator(VADAnnotator("NRC_VAD", "../data/sentiment/lexicons/NRC_VAD_Lexicon/Norwegian-no-NRC-VAD-Lexicon.txt"))
+        self.add_annotator(VADAnnotator("NRC_VAD", root + "data/sentiment/lexicons/NRC_VAD_Lexicon/Norwegian-no-NRC-VAD-Lexicon.txt"))
 
-        self.add_annotator(SocalAnnotator("Socal-adj", "../data/sentiment/lexicons/socal/no_adj.txt"))
+        self.add_annotator(SocalAnnotator("Socal-adj", root + "data/sentiment/lexicons/socal/no_adj.txt"))
 
-        self.add_annotator(SocalAnnotator("Socal-adv", "../data/sentiment/lexicons/socal/no_adv.txt"))
+        self.add_annotator(SocalAnnotator("Socal-adv", root + "data/sentiment/lexicons/socal/no_adv.txt"))
 
-        self.add_annotator(SocalAnnotator("Socal-google", "../data/sentiment/lexicons/socal/no_google.txt"))
+        self.add_annotator(SocalAnnotator("Socal-google", root + "data/sentiment/lexicons/socal/no_google.txt"))
 
-        self.add_annotator(SocalAnnotator("Socal-int", "../data/sentiment/lexicons/socal/no_int.txt"))
+        self.add_annotator(SocalAnnotator("Socal-int", root + "data/sentiment/lexicons/socal/no_int.txt"))
 
-        self.add_annotator(SocalAnnotator("Socal-noun", "../data/sentiment/lexicons/socal/no_noun.txt"))
-        self.add_annotator(SocalAnnotator("Socal-verb", "../data/sentiment/lexicons/socal/no_verb.txt"))
+        self.add_annotator(SocalAnnotator("Socal-noun", root + "data/sentiment/lexicons/socal/no_noun.txt"))
+        self.add_annotator(SocalAnnotator("Socal-verb", root + "data/sentiment/lexicons/socal/no_verb.txt"))
 
-        self.add_annotator(SocalAnnotator("IBM", "../data/sentiment/lexicons/IBM_Debater/no_unigram.txt"))
+        self.add_annotator(SocalAnnotator("IBM", root + "data/sentiment/lexicons/IBM_Debater/no_unigram.txt"))
 
-        self.add_annotator(NRC_SentAnnotator("NRC-Sent-Emo", "../data/sentiment/lexicons/NRC_Sentiment_Emotion/no_sent.txt"))
+        self.add_annotator(NRC_SentAnnotator("NRC-Sent-Emo", root + "/data/sentiment/lexicons/NRC_Sentiment_Emotion/no_sent.txt"))
 
-        self.add_annotator(BUTAnnotator("norsent_forms-BUT", "../data/sentiment/lexicons/norsentlex/Fullform"))
+        self.add_annotator(BUTAnnotator("norsent_forms-BUT", root + "/data/sentiment/lexicons/norsentlex/Fullform"))
 
-        self.add_annotator(BUTAnnotator("norsent_lemma-BUT", "../data/sentiment/lexicons/norsentlex/Lemma"))
+        self.add_annotator(BUTAnnotator("norsent_lemma-BUT", root + "/data/sentiment/lexicons/norsentlex/Lemma"))
 
         return self
 
-    def add_ml_models(self):
-        self.add_annotator(DocBOWAnnotator("doc-level-norec", "../data/sentiment/models/bow"))
+    def add_ml_models(self, root):
+        self.add_annotator(DocBOWAnnotator("doc-level-norec", root + "/data/sentiment/models/bow"))
         self.add_annotator(MultilingualAnnotator("nlptown-bert-multilingual-sentiment"))
         self.add_annotator(MBertAnnotator("mbert-sst"))
         return self
